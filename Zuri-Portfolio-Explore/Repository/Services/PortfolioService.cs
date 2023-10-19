ee5ee880aebf0e8976fbbd1c792f49642ae2f779
Bolu <bolubee95@gmail.com>
2023-10-19 13:51:07 +0100

    resolved multiple portfolio

--- Zuri-Portfolio-Explore/Repository/Services/PortfolioService.cs
+++ Zuri-Portfolio-Explore/Repository/Services/PortfolioService.cs
@@ -28,7 +28,7 @@ namespace Zuri_Portfolio_Explore.Repository.Services
 
             // Retrieve user items from DB
             var usersQuery = _context.Users.Include(u => u.SkillDetails).Include(u => u.Projects).Include(u => u.UserTrack)
-				.ThenInclude(u => u.Track);
+                .ThenInclude(u => u.Track);
 
             var users = await usersQuery
                 .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
@@ -43,7 +43,7 @@ namespace Zuri_Portfolio_Explore.Repository.Services
             foreach (var item in users)
             {
                 var portfolioResponse = MapToResponse(item);
-				portfolioResponses.Add(portfolioResponse);
+                portfolioResponses.Add(portfolioResponse);
             }
 
             return PaginationHelper.CreatePagedReponse(portfolioResponses, validFilter, usersCount, _uriService, route, "Items retrieved successfully");
@@ -55,12 +55,12 @@ namespace Zuri_Portfolio_Explore.Repository.Services
             var route = _httpContextAccessor.HttpContext.Request.Path.Value;
 
             var query = _context.Users
-           // .Include(x => x.UserRoles)
+            // .Include(x => x.UserRoles)
             .Include(x => x.SkillDetails)
             .Include(x => x.Projects)
-			.Include(u => u.UserTrack)
-				.ThenInclude(u => u.Track)
-			.AsQueryable(); // Start with IQueryable
+            .Include(u => u.UserTrack)
+                .ThenInclude(u => u.Track)
+            .AsQueryable(); // Start with IQueryable
 
             if (portfolioFilterDTO.Skill is not null)
             {
@@ -75,7 +75,7 @@ namespace Zuri_Portfolio_Explore.Repository.Services
             if (portfolioFilterDTO.Track is not null)
             {
                 var trackLower = portfolioFilterDTO.Track.Trim().ToLower();
-                query = query.Where(x => x.UserTrack.Track.track.ToLower() == trackLower);
+                query = query.Where(x => x.UserTrack.Any(x => x.Track.track.ToLower() == trackLower));
             }
             //if (portfolioFilterDTO.Ranking is not null)
             //{
@@ -153,12 +153,12 @@ namespace Zuri_Portfolio_Explore.Repository.Services
             var portfolioResponseQuery = _context.Users
                 .Include(u => u.SkillDetails)
                 .Include(u => u.Projects)
-				.Include(u => u.UserTrack)
-				.ThenInclude(u => u.Track)
-				//.Include(u => u.UserRoles)
-				//.ThenInclude(x => x.Role)
-				.Where(x => x.FirstName.ToLower().Contains(searchTerm) || x.LastName.ToLower().Contains(searchTerm)
-                || x.Username.ToLower().Contains(searchTerm))
+                .Include(u => u.UserTrack)
+                .ThenInclude(u => u.Track)
+                //.Include(u => u.UserRoles)
+                //.ThenInclude(x => x.Role)
+                .Where(x => x.FirstName.ToLower().Contains(searchTerm) || x.LastName.ToLower().Contains(searchTerm)
+                || x.Username.ToLower().Contains(searchTerm) || x.UserTrack.Any(x => x.Track.track.ToLower().Contains(searchTerm)))
                 .Select(x => MapToResponse(x));
             var portfolioResponses = await portfolioResponseQuery
                 .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
@@ -178,36 +178,36 @@ namespace Zuri_Portfolio_Explore.Repository.Services
                 .Include(u => u.SkillDetails)
                 .Include(u => u.Projects)
                 .Include(u => u.UserTrack)
-                .ThenInclude(u =>u.Track)
-               // .ThenInclude(x => x.Role)
+                .ThenInclude(u => u.Track)
+                // .ThenInclude(x => x.Role)
                 .FirstOrDefaultAsync(x => x.Id == userId);
             if (portfolio == default)
                 return ApiResponse<PortfolioResponse>.Fail("User not found", 404);
             var userPortfolio = MapToResponse(portfolio);
-                return ApiResponse<PortfolioResponse>.Success("Portfolio Retrieved", userPortfolio);
+            return ApiResponse<PortfolioResponse>.Success("Portfolio Retrieved", userPortfolio);
         }
 
         private static PortfolioResponse MapToResponse(User item)
         {
             return new PortfolioResponse()
-			{
-				Id = item.Id.ToString(),
-				ProfilePictureUrl = item.ProfilePicture,
-				ProfileUrl = Shared.ProfileUrl(item.Id),
-				FirstName = item.FirstName,
-				LastName = item.LastName,
-				Address = item.Location == string.Empty && item.Country == string.Empty
-						? " "
-						: string.Concat(item.Location, ", ", item.Country),
-				Provider = item.Provider,
-				Location = item.Location,
-				Track = item.UserTrack?.Track?.track,
-				//Ranking = item.Ranking,
-				//Tag = item.Tag,
-				Skills = item.SkillDetails.Select(m => m.Skills).ToList(), //Gets user skills
-				Projects = item.Projects.Select(m => m.Id).ToList().Count, //Gets user total project
-        		CreatedAt = item.CreatedAt,
-			};
-		}
+            {
+                Id = item.Id.ToString(),
+                ProfilePictureUrl = item.ProfilePicture,
+                ProfileUrl = Shared.ProfileUrl(item.Id),
+                FirstName = item.FirstName,
+                LastName = item.LastName,
+                Address = item.Location == string.Empty && item.Country == string.Empty
+                        ? " "
+                        : string.Concat(item.Location, ", ", item.Country),
+                Provider = item.Provider,
+                Location = item.Location,
+                Track = item.UserTrack.Select(x => x.Track.track).ToList(),
+                //Ranking = item.Ranking,
+                //Tag = item.Tag,
+                Skills = item.SkillDetails.Select(m => m.Skills).ToList(), //Gets user skills
+                Projects = item.Projects.Select(m => m.Id).ToList().Count, //Gets user total project
+                CreatedAt = item.CreatedAt,
+            };
+        }
     }
 }
